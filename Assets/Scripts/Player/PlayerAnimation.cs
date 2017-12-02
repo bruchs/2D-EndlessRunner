c6136698fffd7161031bc3a4dd6fa882567a79c9
Matias Fuentes <matias.f_c@outlook.es>
2017-12-02 01:18:05 -0800

    Base Project, Basic Player Movement & Art

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator mAnimator;
+    private Rigidbody2D mRigidbody;
+
+    private void Start()
+    {
+        SetInitialReferences();
+    }
+
+    private void SetInitialReferences()
+    {
+        mAnimator = GetComponent<Animator>();
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        SetCurrentAnimation();
+    }
+
+    private void SetCurrentAnimation()
+    {
+        // TODO Check For Player State (Dead Or Alive).
+        // TODO Check For Player Grounded.
+
+        // If Player Speed Is Greater Than 0 and Player Is Not Jumping
+        if (mRigidbody.velocity.x > 0.0F)
+        {
+            mAnimator.SetBool("IsRunning", true);
+        }
+        else
+            mAnimator.SetBool("IsRunning", false);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D mRigidbody;
+    public Vector2 movementVector;
+    public float movementSpeed;
+    public float jumpForce;
+
+    private void Start()
+    {
+        SetInitialReferences();
+    }
+
+    private void SetInitialReferences()
+    {
+        mRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Manages Player Movement
+    private void FixedUpdate()
+    {
+        Run();
+        IsGrounded();
+    }
+
+    // Manages Player Input
+    private void Update()
+    {
+        Jump();
+    }
+
+    private void Run()
+    {
+        transform.position += transform.right * (movementSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 nextPosition = transform.up * jumpForce;
+            mRigidbody.AddForce(nextPosition, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 RayPosition = new Vector3(transform.position.x, transform.position.y - 0.525F, transform.position.z);
+        Debug.DrawRay(RayPosition, -transform.up, Color.red);
+
+        if (Physics.Raycast(RayPosition, -transform.up, 0.5F))
+        {
+            print("***Grounded Status: true");
+            return true;
+        }
+        print("***Grounded Status: false");
+        return false;
+    }
+}
